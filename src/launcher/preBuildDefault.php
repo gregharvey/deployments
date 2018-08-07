a32a787b3671d46c4eb05274e05311a442c1106e
gregharvey <greg.harvey@gmail.com>
2018-08-07 19:32:02 +0200

    Adding a preBuildDefault class.

---
+++ src/launcher/preBuildDefault.php
@@ -0,0 +1,11 @@
+<?php
+namespace CodeEnigma\Deployments\launcher;
+
+class preBuildDefault {
+  public function __construct() {
+
+  }
+  public function process() {
+    print "Hello World!";
+  }
+}
